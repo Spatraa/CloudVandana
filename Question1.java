27042faf0447a11a8e7d364a8193cb37b79581ac
sankar patra <sankarpatra573@gmail.com>
2023-10-30 11:43:41 +0530

    done

---
+++ Question1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Question1 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(array);
+        printArray(array);
+    }
+
+    public static void shuffleArray(int[] array) {
+        Random random = new Random();
+
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+
+            // Swap array[i] and array[j]
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ Question2.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase();
+        scanner.close();
+
+        int result = romanToInt(romanNumeral);
+        System.out.println("The integer value of " + romanNumeral + " is " + result);
+    }
+
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanMap.get(s.charAt(i));
+
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+}
